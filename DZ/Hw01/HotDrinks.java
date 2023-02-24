3401da888699280b5b04776d70ce69f0ceb3e3a3
unknown <levin70@outlook.com>
2023-02-25 00:53:39 +0700

    Remame DZ_1

---
+++ DZ/Hw01/HotDrinks.java
@@ -0,0 +1,16 @@
+package DZ.Hw01;
+
+public class HotDrinks extends Product {
+    private int volume;
+
+    public HotDrinks(String name, double cost, int volume) {
+        super(name, cost);
+        this.volume = volume;
+    }
+
+    @Override
+    public String toString(){
+        return String.format("наименование - %s, цена - %.2f руб., объем - %d мл.", name, cost, volume);
+    }
+}
+    
