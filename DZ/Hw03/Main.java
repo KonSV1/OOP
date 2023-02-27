2b79fd2d3b7dcc1a7fe7c9916be58ecbf41547f7
unknown <levin70@outlook.com>
2023-02-27 21:16:44 +0700

    Creat Hw_03

---
+++ DZ/Hw03/Main.java
@@ -0,0 +1,27 @@
+package DZ.Hw03;
+
+public class Main {
+    public static void main(String[] args) {
+        LinkedList test = new LinkedList();
+        test.add(1);
+        test.add(2);
+        test.add(4);
+        test.add( 61);
+        test.add(5);
+        test.add(5);
+        test.add(18);
+    
+        test.print();
+        System.out.println("Длина списка " + test.size() + " элементов");
+        test.remove(5);
+        test.print();
+        System.out.println("Длина списка " + test.size() + " элементов");
+        test.add(0);
+        test.print();
+        System.out.println("Длина списка " + test.size() + " элементов");
+
+        for (var data : test) {
+            System.out.println(data);
+        }
+    }
+}
