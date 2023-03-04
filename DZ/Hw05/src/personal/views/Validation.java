30c701fa7a7214ca3b1f572d36f3a6f4c338d8b6
levin <70Levin@gmail.com>
2023-03-05 06:02:20 +0700

    Create files

---
+++ DZ/Hw05/src/personal/views/Validation.java
@@ -0,0 +1,26 @@
+package personal.views;
+
+import personal.model.User;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Validation {
+    Pattern namePattern = Pattern.compile("^\\S+$");
+    Pattern phonePattern = Pattern.compile("^((8|\\+7)[\\-]?)?(\\(?\\d{3}\\)?[\\-]?)?[\\d\\-]{7,10}$");
+    public void validateUser(User inputUser) throws Exception {
+        Matcher matcherFName = namePattern.matcher(inputUser.getFirstName());
+        Matcher matcherLName = namePattern.matcher(inputUser.getLastName());
+        Matcher matcherPhone = phonePattern.matcher(inputUser.getPhone());
+        if(!matcherFName.find()){
+            throw new Exception("Имя содержит пробелы");
+        }
+        if(!matcherLName.find()){
+            throw new Exception("Фамилия содержит пробелы");
+        }
+        if(!matcherPhone.find()){
+            throw new Exception("Номер телефона содержит нецифровые символы");
+        }
+
+    }
+}
